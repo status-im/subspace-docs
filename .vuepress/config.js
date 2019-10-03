c27aae5db3702d995ff2b8d9dedec58afdbe2e38
Richard Ramos <info@richardramos.me>
2019-10-03 14:59:25 -0400

    feat: added favicon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,17 @@ module.exports = {
   title: 'SUBSPACE',
   description: 'Reactive ÐApp Development',
   base: '/subspace-docs/',
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/img/logo-small.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/img/logo-small.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/img/logo-small.png"}],
+    ['link', { rel: "shortcut icon", href: "/assets/img/logo-small.png"}],
+    ['meta', { name: "theme-color", content: "#ffffff"}],
+    // ['link', { rel: "manifest", href: "/assets/favicons/site.webmanifest"}],
+    // ['link', { rel: "mask-icon", href: "/assets/favicons/safari-pinned-tab.svg", color: "#3a0839"}],
+    // ['meta', { name: "msapplication-TileColor", content: "#3a0839"}],
+    // ['meta', { name: "msapplication-config", content: "/assets/favicons/browserconfig.xml"}],
+  ],
   markdown: {
     lineNumbers: true
   },
